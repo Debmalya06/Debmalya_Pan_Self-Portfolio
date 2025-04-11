b8649a012db5025da56ced0da4200a1cd7842cb7
Deb006P <debmalyapan4@gmail.com>
2025-04-12 02:23:59 +0530

    update port, server

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     setIsSubmitting(true)
 
     try {
-      const response = await fetch("http://localhost:5000/api/email", {
+      const response = await fetch("https://service-r6d8.onrender.com:10000/api/email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
