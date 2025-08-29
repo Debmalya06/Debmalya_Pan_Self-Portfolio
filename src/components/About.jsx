b7f3258657614f4369e479b7f43c7155950ed8b6
Deb006P <debmalyapan4@gmail.com>
2025-08-29 20:36:40 +0530

    add new file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,7 +61,7 @@ const About = () => {
             </div> */}
 
             <a
-              href="https://drive.google.com/file/d/1SFUOeGeYBAZRk7jKenXyaLoAcQBSFbiC/view?usp=drivesdk"
+              href="https://drive.google.com/file/d/1DhUTmkPOmH5wEpOzUCneO6ztdBKXejwL/view?usp=sharing"
               className="inline-flex items-center gap-2 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition-all duration-300 shadow-lg shadow-blue-600/20"
             >
               <FaDownload /> Download CV
